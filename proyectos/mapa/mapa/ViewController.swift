c6a9f7a75b9b748d356715f2de765208a59e9e27
Francisco Javier Omedes Alrich <javieralrich@gmail.com>
2021-05-08 10:57:41 -0500

    agregar ultimos proyectos

--- proyectos/mapa/mapa/ViewController.swift
+++ proyectos/mapa/mapa/ViewController.swift
@@ -16,9 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let barcelona = Ciudad(titulo: "Barcelona", coordinate: CLLocationCoordinate2D(latitude: 41.43107335002477, longitude: 2.1089402780978967), info: "Ciudad de España")
-        let cdmx = Ciudad(titulo: "CDMX", coordinate: CLLocationCoordinate2D(latitude: 19.4978, longitude: -99.1269), info: "Capital de México")
-        let leon = Ciudad(titulo: "León Gto", coordinate: CLLocationCoordinate2D(latitude: 21.116667, longitude: -101.683334), info: "Ciudad de Guanajuato")
+        let barcelona = Ciudad(title: "Barcelona", coordinate: CLLocationCoordinate2D(latitude: 41.43107335002477, longitude: 2.1089402780978967), info: "Ciudad de España")
+        let cdmx = Ciudad(title: "CDMX", coordinate: CLLocationCoordinate2D(latitude: 19.4978, longitude: -99.1269), info: "Capital de México")
+        let leon = Ciudad(title: "León Gto", coordinate: CLLocationCoordinate2D(latitude: 21.116667, longitude: -101.683334), info: "Ciudad de Guanajuato")
         
         mapa.addAnnotations([barcelona, cdmx, leon])
     }
